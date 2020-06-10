dfdff38e18ae26e9dbfb3bdff87ed6c845f18a72
sbacha <sam@freighttrust.com>
2020-06-09 22:21:08 -0700

    feat(release canidate for documentation, v3.0.1): 3.0.1-rc1
    BREAKING CHANGE: no longer using markdown nor mkdocs. all documentation should be fully asciidoc
    compliant by end of month
    
    issues #12, #15, and #21

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,6 @@ const { parallel, series, watch } = require('gulp')
 const createTask = require('./gulp.d/lib/create-task')
 const exportTasks = require('./gulp.d/lib/export-tasks')
 const log = require('fancy-log')
-const { watch, series, parallel, src, dest, gulp } = require('gulp');
-const connect = require('gulp-connect'); // Runs a local webserver
-const open = require('gulp-open'); // Opens a URL in a web browser
-
-// Launch Chrome web browser
-// https://www.npmjs.com/package/gulp-open
-function openBrowser(done) {
-    var options = {
-    uri: 'http://localhost:8080'
-    };
-    return src('./')
-    .pipe(open(options));
-    done();
-}
-
-// Gulp plugin to run a webserver (with LiveReload)
-// https://www.npmjs.com/package/gulp-connect
-function server(done) {
-    return connect.server({
-    root: './',
-    port: 8080,
-    debug: true,
-    });
-    done();
-}
-
-// Default Gulp command
-exports.default = series(openBrowser, server);
 
 const bundleName = 'ui'
 const buildDir = 'build'
